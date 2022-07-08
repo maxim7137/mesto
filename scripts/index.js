aa2fd29aa15937aeec3e3b026355d948c4841254
maxim713 <maxim713@mail.ru>
2022-07-08 15:21:21 +0300

    fix: исправить type button

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,64 @@
+// КОНСТАНТЫ //
+
+// Находим кнопки
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 const saveButton = document.querySelector('.popup__submit-button');
 const popup = document.querySelector('.popup');
 
-function popupOpen(evt) {
-  // Выберите элементы, откуда должны быть вставлены значения полей
-  let profileName = document.querySelector('.profile__name');
-  let profileCharacter = document.querySelector('.profile__character');
+// ПЕРЕМЕННЫЕ //
 
-  // Находим форму в DOM
-  let formElement = document.querySelector('.popup__container');
-  // Находим поля формы в DOM
-  let nameInput = formElement.querySelector('#user-name');
-  let jobInput = formElement.querySelector('#user-character');
-
-  // Вставьте новые значения с помощью textContent
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileCharacter.textContent;
-
-
-  evt.preventDefault();
-  popup.classList.add('popup_opened');
-}
-
-function popupClose(evt) {
-  evt.preventDefault();
-  popup.classList.remove('popup_opened');
-}
-
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose);
-
-// Находим форму в DOM
+// Находим форму
 let formElement = document.querySelector('.popup__container');
-// Находим поля формы в DOM
+
+// Находим поля формы
 let nameInput = formElement.querySelector('#user-name');
 let jobInput = formElement.querySelector('#user-character');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
+// Находим элементы, откуда должны быть вставлены значения полей
+let profileName = document.querySelector('.profile__name');
+let profileCharacter = document.querySelector('.profile__character');
 
-  // Получите значение полей jobInput и nameInput из свойства value
-  let nameInputText = nameInput.value;
-  let jobInputText = jobInput.value;
-  // Выберите элементы, куда должны быть вставлены значения полей
-  let profileName = document.querySelector('.profile__name');
-  let profileCharacter = document.querySelector('.profile__character');
-  // Вставьте новые значения с помощью textContent
-  profileName.textContent = nameInputText;
-  profileCharacter.textContent = jobInputText;
+// ФУНКЦИИ //
+
+// Открываем попап
+function popupOpen() {
+  // Вставляем значения из документа в поля формы с помощью textContent
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileCharacter.textContent;
+  popup.classList.add('popup_opened');
+}
+
+// Закрываем попап
+function popupClose() {
   popup.classList.remove('popup_opened');
 }
+
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+  // Вставляем новые значения из полей в документ с помощью textContent
+  profileName.textContent = nameInput.value;
+  profileCharacter.textContent = jobInput.value;
+  popupClose();
+}
+
+// СЛУШАТЕЛИ СОБЫТИЙ //
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
-// formElement.addEventListener('keydown', formSubmitHandler);
+
+// Открываем попап редактирования профиля по клику на кнопку
+editButton.addEventListener('click', popupOpen);
+// Закрываем попап редактирования профиля по клику на кнопку
+closeButton.addEventListener('click', popupClose);
+
+/*
+// Сохраняем введенные данные по нажатию клавиши Enter (formElement.addEventListener('keydown', formSubmitHandler);)
 formElement.addEventListener('keydown', function (event) {
   if (event.code === 'Enter') {
     formSubmitHandler(event)
   }
-});
+}); */
