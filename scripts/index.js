3358f7b755ed2340788f4e282c1bbd4ff00faf43
maxim713 <maxim713@mail.ru>
2022-07-18 19:12:27 +0300

    feature: сделать одну функцию на все кнопки закрытия и за попапом

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,29 @@
 // КОНСТАНТЫ //
 const root = document.querySelector('.page');
 // Находим кнопки
-const editButton = document.querySelector('.profile__edit-button');
-const saveButton = document.querySelectorAll('.popup__submit-button')[0];
+const editButton = root.querySelector('.profile__edit-button');
+const saveButton = root.querySelectorAll('.popup__submit-button')[0];
 
-const addButton = document.querySelector('.profile__add-button');
-const saveButtonCard = document.querySelectorAll('.popup__submit-button')[1];
+const addButton = root.querySelector('.profile__add-button');
+const saveButtonCard = root.querySelectorAll('.popup__submit-button')[1];
 
 // Находим попапы
-const popupProfile = document.querySelector('.popup-profile'); // попап редактирования профиля
-const popupCard = document.querySelector('.popup-card'); // попап добавления карточки
+const popupProfile = root.querySelector('.popup-profile'); // попап редактирования профиля
+const popupCard = root.querySelector('.popup-card'); // попап добавления карточки
 
 // ПЕРЕМЕННЫЕ //
 
 // Находим форму
-let formElement = document.querySelectorAll('.popup__container')[0];
-let formElementCard = document.querySelectorAll('.popup__container')[1];
+let formElement = root.querySelectorAll('.popup__container')[0];
+let formElementCard = root.querySelectorAll('.popup__container')[1];
 
 // Находим поля формы
 let nameInput = formElement.querySelector('.popup__text_user_name');
 let jobInput = formElement.querySelector('.popup__text_user_character');
 
 // Находим элементы, откуда должны быть вставлены значения полей
-let profileName = document.querySelector('.profile__name');
-let profileCharacter = document.querySelector('.profile__character');
+let profileName = root.querySelector('.profile__name');
+let profileCharacter = root.querySelector('.profile__character');
 
 // ФУНКЦИИ //
 
@@ -39,19 +39,30 @@ function popupClose(p) {
 
 // Функция закрытия попапа для всех кнопок закрытия
 function closePopup(evt) {
-  const closeBtn = evt.target;
-  const modal = closeBtn.closest('.popup');
-  if (closeBtn.classList.contains('popup_close')) {
+  const target = evt.target;
+  const modal = target.closest('.popup');
+  if (target.classList.contains('popup_close') || target === modal) {
     popupClose(modal);
   }
 }
 
+// Функция вставки значений из полей в документ
+function insertValuesFromField() {
+  profileName.textContent = nameInput.value;
+  profileCharacter.textContent = jobInput.value;
+}
+
+// Функция вставки значений из документа в поле
+function insertValuesToField() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileCharacter.textContent;
+}
+
+
 // Обработчик «отправки» формы, профиля
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Вставляем новые значения из полей в документ с помощью textContent
-  profileName.textContent = nameInput.value;
-  profileCharacter.textContent = jobInput.value;
+  insertValuesFromField(); // Вставляем новые значения из полей в документ с помощью textContent
   popupClose(popupProfile);
 }
 
@@ -62,17 +73,16 @@ function formSubmitHandlerCard(evt) {
 }
 
 // СЛУШАТЕЛИ СОБЫТИЙ //
+// обработчик кликов для закрытия любых попапов
 root.addEventListener('click', closePopup);
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
-formElementCard.addEventListener('submit', formSubmitHandlerCard);
+
+// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); // форма профиля
+formElementCard.addEventListener('submit', formSubmitHandlerCard); // форма карточки
 
 // Открываем попап редактирования профиля по клику на кнопку
 editButton.addEventListener('click', () => {
-  // Вставляем значения из документа в поля формы с помощью textContent
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileCharacter.textContent;
+  insertValuesToField(); // Вставляем значения из документа в поля формы с помощью textContent
   popupOpen(popupProfile);
 });
 
